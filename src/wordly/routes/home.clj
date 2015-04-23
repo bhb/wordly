9c431e95bc3a5073456210b1891ffe96e9bb2dfe
Ben Brinckerhoff <ben@bbrinck.com>
2015-04-22 20:35:02 -0700

    Adds GET endpoint for testing, hooks up basic table

--- src/wordly/routes/home.clj
+++ src/wordly/routes/home.clj
@@ -1,5 +1,6 @@
 (ns wordly.routes.home
   (:require [compojure.core :refer :all]
+            [wordly.word-count :as wc]
             [wordly.views.layout :as layout]))
 
 (defn home []
@@ -11,14 +12,28 @@
                   ]
                  ))
 
+(defn row [pair]
+  (let [[word count] pair]
+    [:tr
+     [:td word]
+     [:td count]
+     ]))
+
+(defn table [frequencies]
+  [:table
+   [:tr
+    [:th "Word"]
+    [:th "Occurences"]]
+   (map row frequencies)
+   ])
+
 (defn new-word-count [request]
   (let [url (get-in request [:params :url])]
-    (println request)
-    (layout/common [:h1 "Words for" url]
-                   [:p url]
-                   )))
+    (layout/common [:h1 "Words for " url]
+                   (table (wc/top-words url)))))
 
 (defroutes home-routes
   (GET "/" [] (home))
+  (GET "/word-counts" request (new-word-count request))
   (POST "/word-counts" request (new-word-count request))
   )
